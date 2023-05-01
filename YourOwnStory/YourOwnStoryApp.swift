75bb4827a97cb0223d9fdf4629168a54d87cd00d
ademclk <ademonurcelik@icloud.com>
2023-05-01 19:56:29 +0300

    Initial Commit

---
+++ YourOwnStory/YourOwnStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourOwnStoryApp.swift
+//  YourOwnStory
+//
+//  Created by Adem Onur Çelik on 1.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct YourOwnStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
